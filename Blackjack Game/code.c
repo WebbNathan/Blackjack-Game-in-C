09c5c160fec84fa913860918b904d0b1f3bc025c
Nathan Webb <webbnathana@gmail.com>
2024-04-09 13:12:32 -0700

    Add project files.

---
+++ Blackjack Game/code.c
@@ -0,0 +1,211 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+typedef struct Card_struct {
+	int suit;
+	int value;
+	char suitName[9];
+	char valueName[7];
+} Card;
+
+Card createCardFunc(int i, int j);
+void createDeckFunc(Card deck[]);
+void deckShuffle(Card deck[]);
+void listPop(int popIndex, int len, Card deck[]);
+void cardAllocate(Card deck[], Card p_hand[], Card d_hand[]);
+void printHands(Card hand[], int len);
+void hitFunc(Card deck[], Card p_hand[], int* p_handSize);
+bool bustCheck(Card hand[], int len);
+bool compSumCheck(Card hand[], int len);
+bool mainGame(); //this will return whether the user wants to play again
+
+int main() {
+
+	while (mainGame());
+	printf("Thanks for playing!");
+
+	return 0;
+}
+
+Card createCardFunc(int i, int j) {
+	Card card;
+	card.suit = i;
+	card.value = j;
+	char valueChar[3];
+	switch (card.suit) {
+	case 0:
+		strcpy(card.suitName, "Hearts");
+		break;
+	case 1:
+		strcpy(card.suitName, "Clubs");
+		break;
+	case 2:
+		strcpy(card.suitName, "Diamonds");
+		break;
+	case 3:
+		strcpy(card.suitName, "Spades");
+		break;
+	}
+
+	if (card.value == 1 || card.value > 9) {
+		switch (card.value) {
+		case 1:
+			strcpy(card.valueName, "Ace\0");
+			card.value = 11;
+			break;
+		case 11:
+			strcpy(card.valueName, "Jack\0");
+			card.value = 10;
+			break;
+		case 12:
+			strcpy(card.valueName, "Queen\0");
+			card.value = 10;
+			break;
+		case 13:
+			strcpy(card.valueName, "King\0");
+			card.value = 10;
+			break;
+		}
+	}
+	else {
+		sprintf(valueChar, "%d", card.value);
+		strcpy(card.valueName, valueChar);
+	}
+	return card;
+}
+
+void createDeckFunc(Card deck[]) {
+	int cardIter = 0;
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 1; j < 14; j++) {
+			deck[cardIter] = createCardFunc(i, j);
+			cardIter++;
+		}
+	}
+}
+
+void deckShuffle(Card deck[]) {
+	int randInt;
+	Card temp;
+	time_t t;
+
+	srand((unsigned)time(&t));
+	//info on rand, and time libraries obained from TutorialsPoint -> https://www.tutorialspoint.com
+
+	for (int i = 0; i < 52; i++) {
+		randInt = rand() % 52;
+		temp = deck[i];
+		deck[i] = deck[randInt];
+		deck[randInt] = temp;
+	}
+}
+
+void listPop(int popIndex, int len, Card deck[]) {
+	//printf("Popping %s of %s\n", deck[popIndex].valueName, deck[popIndex].suitName);
+	for (int i = 0; i < len; i++) {
+		deck[i] = deck[i + 1];
+	}
+}
+
+void cardAllocate(Card deck[], Card p_hand[], Card d_hand[]) {
+	for (int i = 0; i < 2; i++) {
+		p_hand[i] = deck[0];
+		listPop(0, 52, deck);
+	}
+	for (int i = 0; i < 2; i++) {
+		d_hand[i] = deck[0];
+		listPop(0, 52, deck);
+	}
+}
+
+void printHands(Card hand[], int len) {
+	for (int i = 0; i < len; i++) {
+		printf("| %s of %s | ", hand[i].valueName, hand[i].suitName);
+	}
+	printf("\n");
+}
+
+void hitFunc(Card deck[], Card p_hand[], int* p_handSize) {
+	p_hand[(*p_handSize)] = deck[0];
+	listPop(0, 52, deck);
+}
+
+bool bustCheck(Card hand[], int len) {
+	int sum;
+	bool restart = true;
+	while (restart) {
+		sum = 0;
+		for (int i = 0; i < len; i++) {
+			sum += hand[i].value;
+		}
+		if (sum > 21) {
+			restart = false;
+			for (int i = 0; i < len; i++) {
+				if (hand[i].value == 11) {
+					hand[i].value = 1;
+					restart = true;
+				}
+			}
+		}
+		else {
+			return false;
+		}
+	}
+	return true;
+}
+
+bool compSumCheck(Card hand[], int len) {
+
+}
+
+bool mainGame() {
+	int deckSize = 52, playerHandSize = 2, computerHandSize = 2;
+	Card deckList[52], playerHand[52], computerHand[52];
+	bool done = false, bust = false;
+	char playerChoice[6];
+
+	createDeckFunc(deckList);
+	deckShuffle(deckList);
+	/*for (int i = 0; i < 52; i++) {
+		printf("%s of %s\n", deckList[i].valueName, deckList[i].suitName);
+	} */
+	cardAllocate(deckList, playerHand, computerHand);
+
+	while (!done) {
+		printf("Your hand is: ");
+		printHands(playerHand, playerHandSize);
+		printf("The dealers hand is currently shows as: ");
+		printHands(computerHand, 1);
+		printf("Would you like to Hit, or Stand?: ");
+		scanf("%s", playerChoice);
+
+		if (strcmp("Stand", playerChoice) == 0 || strcmp("stand", playerChoice) == 0) {
+			printf("You've Stood\n");
+			done = true;
+		}
+		else if (strcmp("Hit", playerChoice) == 0 || strcmp("hit", playerChoice) == 0) {
+			hitFunc(deckList, playerHand, &playerHandSize);
+			playerHandSize++;
+			printf("You've hit!\n");
+			bust = bustCheck(playerHand, playerHandSize);
+			if (bust) {
+				printf("And you've bust.\n");
+				printf("You're hand was: ");
+				printHands(playerHand, playerHandSize);
+				done = true;
+			}
+		}
+		else {
+			printf("Please either Hit or Stand\n");
+		}
+
+	}
+
+
+
+	return false;
+}
